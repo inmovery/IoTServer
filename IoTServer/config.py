17b97c187c1aa3befda4b2849b3569e9f0d5e104
Roma Piskunov <roman_piskunov@bk.ru>
2020-09-12 02:10:51 +0300

    fix folder structure

--- IoTServer/config.py
+++
@@ -1,8 +0,0 @@
-DB_HOST = "localhost"
-DB_USERNAME = "g982257e_iot"
-DB_PASSWORD = "Pablo228"
-DB_NAME = "g982257e_iot"
-
-MQTT_UUID = "IoTServer"
-MQTT_USER = "wuadwiew"
-MQTT_PASSWORD = "E2HiQR_4_77X"
